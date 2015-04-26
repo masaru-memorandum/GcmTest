f367a9ee9d7475ccd0bb4c9e69dfac27ee509670
MASARU <masaru@MASARU-MacBook-Air.local>
2015-04-26 22:05:54 +0900

    no message

--- src/com/masaru/gcmtest/GcmBroadcastReceiver.java
+++ src/com/masaru/gcmtest/GcmBroadcastReceiver.java
@@ -1,14 +1,14 @@
 package com.masaru.gcmtest;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
 
-public class GcmBroadcastReceiver extends BroadcastReceiver {
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		
+
 	}
 
 }
--- src/com/masaru/gcmtest/PlayServiceUtil.java
+++ src/com/masaru/gcmtest/PlayServiceUtil.java
@@ -1,5 +1,29 @@
 package com.masaru.gcmtest;
 
-public class PlayServiceUtil {
+import android.app.Activity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+public class PlayServiceUtil {
+	public static boolean isGooglePlayServiceAvailable(Activity activity) {
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(activity);
+		if (resultCode != ConnectionResult.SUCCESS) {
+			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+				GooglePlayServicesUtil.getErrorDialog(resultCode, activity, 0)
+						.show();
+			} else {
+				activity.finish();
+			}
+			return false;
+		}
+		return true;
+	}
+	
+	public static void initializeGcm(Activity activity){
+		GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(activity);
+		
+	}
 }
